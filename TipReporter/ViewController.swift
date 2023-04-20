775f6d7feae1b11d50d6791e88eca33612db63eb
SoupyOfficial <jscampbell21@outlook.com>
2023-04-19 20:13:16 -0400

    Initial Commit

---
+++ TipReporter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipReporter
+//
+//  Created by Soupy Campbell on 4/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
